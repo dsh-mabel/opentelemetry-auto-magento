20ac30ced3a0a54960b38ce29d62cc101e353f21
Dave Smith-Hayes <dave.smithhayes@mabelslabels.com>
2026-02-17 12:30:02 -0500

    Update deprecations for Bootstrap Populate Autoloader hook.

--- src/Hooks/BootstrapPopulateAutoloaderHook.php
+++ src/Hooks/BootstrapPopulateAutoloaderHook.php
@@ -9,6 +9,7 @@ use Codewave\OpenTelemetry\Magento\Trace\CliAutoRootSpan;
 use Magento\Framework\App\Bootstrap;
 use OpenTelemetry\Context\Context;
 use OpenTelemetry\SemConv\TraceAttributes;
+use OpenTelemetry\SemConv\Attributes\CodeAttributes;
 use Throwable;
 
 use function OpenTelemetry\Instrumentation\hook;
@@ -30,10 +31,9 @@ class BootstrapPopulateAutoloaderHook
                 $builder = $this->instrumentation
                     ->tracer()
                     ->spanBuilder('Bootstrap::populateAutoloader')
-                    ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
-                    ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
-                    ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
-                    ->setAttribute(TraceAttributes::CODE_LINENO, $lineno);
+                    ->setAttribute(CodeAttributes::CODE_FUNCTION_NAME, $function)
+                    ->setAttribute(CodeAttributes::CODE_FILE_PATH, $filename)
+                    ->setAttribute(CodeAttributes::CODE_LINE_NUMBER, $lineno);
 
                 $parent = Context::getCurrent();
 
@@ -49,7 +49,7 @@ class BootstrapPopulateAutoloaderHook
         );
     }
 
-    private function registerRootSpan()
+    private function registerRootSpan(): void
     {
         if ($this->rootSpanRegistered) {
             return;
