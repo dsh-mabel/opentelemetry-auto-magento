15fe74e52e4355103c01bd72b7dc988ce3f49156
Dave Smith-Hayes <dave.smithhayes@mabelslabels.com>
2026-02-17 11:56:18 -0500

    Remove use of Deprecated OpenTel constants

--- src/Trace/AutoRootSpan.php
+++ src/Trace/AutoRootSpan.php
@@ -7,7 +7,11 @@ use OpenTelemetry\API\Globals;
 use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\Context\Context;
 use OpenTelemetry\SDK\Trace\AutoRootSpan as Base;
-use OpenTelemetry\SemConv\TraceAttributes;
+use OpenTelemetry\SemConv\Attributes\ServerAttributes;
+use OpenTelemetry\SemConv\Attributes\UserAgentAttributes;
+use OpenTelemetry\SemConv\Attributes\UrlAttributes;
+use OpenTelemetry\SemConv\Attributes\HttpAttributes;
+use OpenTelemetry\SemConv\Incubating\Attributes\HttpIncubatingAttributes;
 use OpenTelemetry\SemConv\Version;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -33,14 +37,14 @@ class AutoRootSpan extends Base
             ->setSpanKind(SpanKind::KIND_SERVER)
             ->setStartTimestamp((int) ($startTime * ClockInterface::NANOS_PER_SECOND))
             ->setParent($parent)
-            ->setAttribute(TraceAttributes::URL_FULL, (string) $request->getUri())
-            ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getMethod())
-            ->setAttribute(TraceAttributes::HTTP_REQUEST_BODY_SIZE, $request->getHeaderLine('Content-Length'))
-            ->setAttribute(TraceAttributes::USER_AGENT_ORIGINAL, $request->getHeaderLine('User-Agent'))
-            ->setAttribute(TraceAttributes::SERVER_ADDRESS, $request->getUri()->getHost())
-            ->setAttribute(TraceAttributes::SERVER_PORT, $request->getUri()->getPort())
-            ->setAttribute(TraceAttributes::URL_SCHEME, $request->getUri()->getScheme())
-            ->setAttribute(TraceAttributes::URL_PATH, $request->getUri()->getPath())
+            ->setAttribute(UrlAttributes::URL_FULL, (string) $request->getUri())
+            ->setAttribute(HttpAttributes::HTTP_REQUEST_METHOD, $request->getMethod())
+            ->setAttribute(HttpIncubatingAttributes::HTTP_REQUEST_BODY_SIZE, $request->getHeaderLine('Content-Length'))
+            ->setAttribute(UserAgentAttributes::USER_AGENT_ORIGINAL, $request->getHeaderLine('User-Agent'))
+            ->setAttribute(ServerAttributes::SERVER_ADDRESS, $request->getUri()->getHost())
+            ->setAttribute(ServerAttributes::SERVER_PORT, $request->getUri()->getPort())
+            ->setAttribute(UrlAttributes::URL_SCHEME, $request->getUri()->getScheme())
+            ->setAttribute(UrlAttributes::URL_PATH, $request->getUri()->getPath())
             ->startSpan();
         Context::storage()->attach($span->storeInContext($parent));
     }
